4643bad3d626fb42b52114e66a995b03cdb03780
Swift1-creator <kik.ss@yandex.ru>
2026-09-09 00:57:43 +0300

    fix

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const corsProxy = 'https://allorigins.hexlet.app';
 
-const REQUEST_TIMEOUT = 3000;
+const REQUEST_TIMEOUT = 2000;
 
 const isRssContent = (value) => (
   typeof value === 'string'
@@ -8,14 +8,18 @@ const isRssContent = (value) => (
   && /<channel(?:\s|>)/i.test(value)
 );
 
+const createTimeout = (milliseconds) => (
+  new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error('INVALID_RSS'));
+    }, milliseconds);
+  })
+);
+
 export const fetchRss = async (url) => {
   const controller = new AbortController();
 
-  const timeoutId = setTimeout(() => {
-    controller.abort();
-  }, REQUEST_TIMEOUT);
-
-  try {
+  const request = (async () => {
     const params = new URLSearchParams({
       url,
       disableCache: 'true',
@@ -55,23 +59,24 @@ export const fetchRss = async (url) => {
     }
 
     return data.contents;
+  })();
+
+  try {
+    return await Promise.race([
+      request,
+      createTimeout(REQUEST_TIMEOUT),
+    ]);
   } catch (error) {
-    if (error instanceof Error && error.name === 'AbortError') {
-      throw new Error('INVALID_RSS');
+    if (error instanceof Error && error.message === 'INVALID_RSS') {
+      throw error;
     }
 
-    if (
-      error instanceof Error
-      && (
-        error.message === 'INVALID_RSS'
-        || error.message === 'NETWORK_ERROR'
-      )
-    ) {
+    if (error instanceof Error && error.message === 'NETWORK_ERROR') {
       throw error;
     }
 
     throw new Error('NETWORK_ERROR');
   } finally {
-    clearTimeout(timeoutId);
+    controller.abort();
   }
 };
